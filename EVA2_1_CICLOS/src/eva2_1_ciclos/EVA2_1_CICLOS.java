c8c6dde184be625680b826938f44f8d0c88f27f9
wosito853 <L24550310@chihuahua2.tecnm.mx>
2024-10-02 17:19:47 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+ // ciclo for - cuando sabe,os cuantas veces se repetirá una instrucciión
+        // funciona como un contador (inicio, fin(condición) y salto de conteo)
+        // vamos a repetir 10 veces contando del 1 a 10
+        //nosotros estavblecemos el úto de partida y punto de termino
+        for(int i = 1; i <= 10; i++ ){
+            System.out.print(i + "-");
+        }
+        System.out.println("");
+        for(int i = 10; i >= 1; i-- ){
+            System.out.print(i + "-");
+        }
+    
+ 
+        
+        
+        
+        
+        
+    }
+    
+}
